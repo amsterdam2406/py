4dcfef032a63032cfd97ada2d1bf6bcc935635ea
Amsat Lolade <amsatlolade@gmail.com>
2026-05-28 05:40:31 +0800

    Fixed django-simple-history requirement

--- payroll/management/commands/init_superuser.py
+++ payroll/management/commands/init_superuser.py
@@ -4,6 +4,7 @@ import os
 
 User = get_user_model()
 
+
 class Command(BaseCommand):
     help = 'Create superuser if it does not exist'
 
@@ -11,16 +12,57 @@ class Command(BaseCommand):
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
         email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'amsatlolade@gmail.com')
         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Q9vR7xLp2@Tz8Km5WY4D6')
-        
-        if not User.objects.filter(username=username).exists():
-            if password:
-                User.objects.create_superuser(
-                    username=username,
-                    email=email,
-                    password=password
-                )
-                self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created'))
-            else:
-                self.stdout.write(self.style.WARNING('DJANGO_SUPERUSER_PASSWORD not set, skipping superuser creation'))
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            # Ensure permissions are correct
+            user.is_staff = True
+            user.is_superuser = True
+            user.is_active = True
+            user.set_password(password)
+            user.save()
+
+            self.stdout.write(
+                self.style.SUCCESS(f'Superuser "{username}" already exists and was updated')
+            )
+
         else:
-            self.stdout.write(self.style.NOTICE(f'Superuser "{username}" already exists'))
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+
+            self.stdout.write(
+                self.style.SUCCESS(f'Superuser "{username}" created successfully')
+            )
+
+
+
+# from django.core.management.base import BaseCommand
+# from django.contrib.auth import get_user_model
+# import os
+
+# User = get_user_model()
+
+# class Command(BaseCommand):
+#     help = 'Create superuser if it does not exist'
+
+#     def handle(self, *args, **options):
+#         username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+#         email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'amsatlolade@gmail.com')
+#         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Q9vR7xLp2@Tz8Km5WY4D6')
+        
+#         if not User.objects.filter(username=username).exists():
+#             if password:
+#                 User.objects.create_superuser(
+#                     username=username,
+#                     email=email,
+#                     password=password
+#                 )
+#                 self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created'))
+#             else:
+#                 self.stdout.write(self.style.WARNING('DJANGO_SUPERUSER_PASSWORD not set, skipping superuser creation'))
+#         else:
+#             self.stdout.write(self.style.NOTICE(f'Superuser "{username}" already exists'))
